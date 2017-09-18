0b0a5f447154eb5278ad58e07e5f7ea5839158b2
mlelansky <mlelansky@mail.de>
2017-09-18 18:35:14 +0200

    Delete no more used files.

--- voldemort_client/serialization/exception.py
+++
@@ -1,7 +0,0 @@
-from voldemort_client.exception import VoldemortException
-
-class SerializationException(VoldemortException):
-    """
-    This class is the concrete exception class for all serialization errors.
-    """
-    pass
